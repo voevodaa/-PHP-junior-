686729b3a9f330e94c2b0ff2aad493b36f4028c1
voevodaa <101362797+voevodaa@users.noreply.github.com>
2022-04-12 10:28:18 +0300

    Update ajax.php

--- ajax.php
+++ ajax.php
@@ -4,9 +4,9 @@ $username = "root";
 $password = "";
 $dbname = "test_db";
 
-// Create connection
+
 $conn = mysqli_connect($servername, $username, $password, $dbname);
-// Check connection
+
 if (!$conn) {
   die("Connection failed: " . mysqli_connect_error());
 }
