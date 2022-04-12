bd92b684633013ec603627d176c555a4b5c06497
voevodaa <101362797+voevodaa@users.noreply.github.com>
2022-04-12 10:30:12 +0300

    Update cproducts.php

--- cproducts.php
+++ cproducts.php
@@ -8,9 +8,7 @@
  $password = "";
  $dbname = "test_db";
 
-// Create connection
  $conn = mysqli_connect($servername, $username, $password, $dbname);
-// Check connection
 if (!$conn) {
   die("Connection failed: " . mysqli_connect_error());
  } 
