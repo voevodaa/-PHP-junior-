57c61ee61caa0d9b780cbcd23b6a57850985f68e
voevodaa <101362797+voevodaa@users.noreply.github.com>
2022-04-12 10:32:30 +0300

    Update sort_dec.php

--- sort_dec.php
+++ sort_dec.php
@@ -8,9 +8,7 @@
  $password = "";
  $dbname = "test_db";
 
-// Create connection
- $conn = mysqli_connect($servername, $username, $password, $dbname);
-// Check connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
 if (!$conn) {
   die("Connection failed: " . mysqli_connect_error());
  } 
