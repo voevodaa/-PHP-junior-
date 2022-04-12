496ec8e9ea107d45400274d61680eade015997ec
voevodaa <101362797+voevodaa@users.noreply.github.com>
2022-04-12 10:28:39 +0300

    Update ajax_minus.php

--- ajax_minus.php
+++ ajax_minus.php
@@ -4,9 +4,7 @@ $username = "root";
 $password = "";
 $dbname = "test_db";
 
-// Create connection
 $conn = mysqli_connect($servername, $username, $password, $dbname);
-// Check connection
 if (!$conn) {
   die("Connection failed: " . mysqli_connect_error());
 }
