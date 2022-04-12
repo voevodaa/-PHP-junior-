54e9233cb68052346cdaa168ff54e24b300ec76f
voevodaa <101362797+voevodaa@users.noreply.github.com>
2022-04-12 10:29:53 +0300

    Update button_visible.php

--- button_visible.php
+++ button_visible.php
@@ -8,10 +8,8 @@
  $password = "";
  $dbname = "test_db";
 
-// Create connection
  $conn = mysqli_connect($servername, $username, $password, $dbname);
-// Check connection
-if (!$conn) {
+ if (!$conn) {
   die("Connection failed: " . mysqli_connect_error());
  } 
  $result = mysqli_query($conn, "SELECT * FROM `products`");
@@ -28,15 +26,15 @@ if (!$conn) {
 
 <script>
  function mytoggle(num) {
-$("#h" + num).toggle();        /* скрытие записи на html страничке*/ 
+$("#h" + num).toggle();        
 
-    $.ajax({                  /*Запрос на ДБ для скрытия записи путем изменения колонки VISIBLE*/
-    url: '/ajax.php',         /* Куда пойдет запрос */
-    method: 'get',             /* Метод передачи (post или get) */
-    dataType: 'json',          /* Тип данных в ответе (xml, json, script, html). */
-    data: {id: num},     /* Параметры передаваемые в запросе. */
-    success: function(data){   /* функция которая будет выполнена после успешного запроса.  */
-        alert(data);            /* В переменной data содержится ответ от ajax.php. */
+    $.ajax({                  
+    url: '/ajax.php',         
+    method: 'get',            
+    dataType: 'json',         
+    data: {id: num},    
+    success: function(data){   
+        alert(data);            
     }
 });
 }
